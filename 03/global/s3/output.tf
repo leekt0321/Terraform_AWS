608ba07e2eaa1a2ec743d0bffc06c6585208651d
leekt0321 <leekt0321@naver.com>
2024-05-31 14:06:03 +0900

    remote state exam(s3,dynamodb)

---
+++ 03/global/s3/output.tf
@@ -0,0 +1,9 @@
+output "s3_remote_state" {
+  value = aws_s3_bucket.remote_state.arn
+  description = "s3 bucket remote state ARN"
+}
+
+output "dydb_remote_state"{
+  value = aws_dynamodb_table.myLocks.id
+  description = "DynamoDB Table name"
+}
